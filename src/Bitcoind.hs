671d565cdd95660234dd421bfd1689ee8c8ccd4c
Rune K. Svendsen <runesvend@gmail.com>
2016-06-07 13:48:53 +0200

    I forgot about the new Bitcoind module

---
+++ src/Bitcoind.hs
@@ -0,0 +1,53 @@
+module Bitcoind where
+
+import Control.Monad.IO.Class (liftIO, MonadIO)
+-- import Control.Monad.Reader (MonadReader)
+-- import Control.Monad.IO.Class (MonadIO)
+import qualified Data.ByteString as BS
+-- import qualified Data.ByteString.Char8 as C
+import qualified Data.ByteString.Base16 as B16
+import Control.Exception (try)
+import Network.Haskoin.Transaction as HT
+import Network.Haskoin.Crypto as HC
+import qualified Data.Bitcoin.Transaction as Btc
+import Network.HTTP.Client (HttpException (..))
+-- import Network.Bitcoin.RawTransaction
+import Network.Bitcoin.Api.Client (Client, withClient)
+import Network.Bitcoin.Api.Transaction (send)
+import Data.HexString as Hex
+import Data.Text as T
+import qualified Data.Binary as Bin
+import qualified Data.ByteString.Lazy as BL
+
+-- isStatusCodeException 401
+
+bitcoindNetworkSumbitTx tx = withClient "192.168.1.102" 8334
+    (T.pack "john_oliver") (T.pack "KGbv6HvJ5z")
+        (tryBitcoindSubmitToNetwork tx)
+
+tryBitcoindSubmitToNetwork ::
+    MonadIO m =>
+    HT.Tx ->
+    Client
+    -> m (Either String HT.TxHash)
+tryBitcoindSubmitToNetwork tx conn = do
+    res <- liftIO $ try $ send conn $ Btc.decode . fromBytes $ serialize tx
+    case res of
+        Left e -> return $
+            Left $ "Error submitting Bitcoin transaction to network. reason: "
+                ++ show (e :: HttpException)
+        Right txid -> do
+            liftIO . putStrLn $ "Submitted Bitcoin transaction to the network: " ++
+                T.unpack (Hex.toText txid)
+            return $ maybe
+                (Left $ "BUG: failed to parse transaction ID returned by bitcoind: " ++ show txid)
+                Right
+                (HT.hexToTxHash (B16.encode $ toBytes txid))
+
+serialize :: Bin.Binary a => a -> BS.ByteString
+serialize = BL.toStrict . Bin.encode
+
+-- StatusCodeException
+--  (Status {statusCode = 500,
+--  statusMessage = \"Internal Server Error\"})
+--  [(\"Content-Type\",\"application/json\"),(\"Date\",\"Sat, 30 Apr 2016 18:15:25 GMT\"),(\"Content-Length\",\"83\"),(\"X-Response-Body-Start\",\"{\\\"result\\\":null,\\\"error\\\":{\\\"code\\\":-26,\\\"message\\\":\\\"64: too-long-mempool-chain\\\"},\\\"id\\\":1}\\n\"),(\"X-Request-URL\",\"POST http://192.168.1.102:8334/\")] (CJ {expose = []})
