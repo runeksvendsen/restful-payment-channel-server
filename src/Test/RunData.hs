a052b3a8d00a1e3b14ceee260e2b9e318d9d40b0
Rune K. Svendsen <runesvend@gmail.com>
2016-06-11 16:37:16 +0200

    enable HTTP keepalive for Haskell test data runner

--- src/Test/RunData.hs
+++ src/Test/RunData.hs
@@ -3,17 +3,21 @@ module Test.RunData where
 import Test.GenData hiding (main)
 import qualified Data.Aeson as JSON (eitherDecodeStrict)
 import qualified Data.ByteString as BS
-import qualified Network.Wreq as Wreq (get, post, put, delete, asJSON, responseBody)
+import qualified Network.Wreq as Wreq (asJSON, responseBody) --get, post, put, delete
+import qualified Network.Wreq.Session as Wreq (post, put, withAPISession)
 import Data.String.Conversions (cs)
 import Control.Monad (forM_)
 
-main :: IO ()
-main = do
-    eitherPaySess <- fmap JSON.eitherDecodeStrict BS.getLine
-    (PaySessionData openURL payURLList) <-
-        case eitherPaySess of
-            Left e -> fail $ "failed to decode JSON test data from stdin: " ++ e
-            Right pd -> return pd
 
-    Wreq.post (cs openURL) BS.empty
-    forM_ (map cs payURLList) (`Wreq.put` BS.empty)
+
+main :: IO ()
+main = Wreq.withAPISession $
+    \conn -> do
+        eitherPaySess <- fmap JSON.eitherDecodeStrict BS.getLine
+        (PaySessionData openURL payURLList) <-
+            case eitherPaySess of
+                Left e -> fail $ "failed to decode JSON test data from stdin: " ++ e
+                Right pd -> return pd
+
+        Wreq.post conn (cs openURL) BS.empty
+        forM_ (map cs payURLList) (flip (Wreq.put conn) BS.empty)
