9b3f74f8436a53cd78287583d984287d2319e3d7
Rune K. Svendsen <runesvend@gmail.com>
2016-06-21 09:50:13 +0200

    keter

--- mkKeter.sh
+++ mkKeter.sh
@@ -5,5 +5,5 @@ BIN="PayChanServer"
 stack build
 cp .stack-work/install/x86_64-linux/lts-5.2/7.10.3/bin/$BIN ./
 strip $BIN
-tar czfv paychan.keter $BIN dist paychan.cfg
+tar czfv paychan.keter $BIN dist config paychan.cfg
 
