e00879d96030857d9fb49528763c2fca821ca6d8
Rune K. Svendsen <runesvend@gmail.com>
2016-06-21 11:43:31 +0200

    Revert "revert keter main logic for now"
    This reverts commit 625678ccb8c91894a40aec73e0674fe17edecc82.

--- mkKeter.sh
+++ mkKeter.sh
@@ -1,33 +1,9 @@
 #!/bin/bash
 
-set -e
-
-CFGFILE="paychan.cfg"
 BIN="PayChanServer"
-DEPLOYDIR="/opt/keter/incoming/"
-
-function mkNetConfString {
-   echo "bitcoin { network = \"$1\" }"
-}
-
-# arg1: "deploy" or something else
-# arg2: filename
-function maybeDeploy {
-   if [ "$1" == "deploy" ]; then
-      cp "$2" "$DEPLOYDIR"
-   fi
-}
 
 stack build
 cp .stack-work/install/x86_64-linux/lts-5.2/7.10.3/bin/$BIN ./
 strip $BIN
-
-for NET in $(echo "live test"); do
-   cp $CFGFILE.nonet $CFGFILE
-   CONFSTR="$(mkNetConfString $NET)"
-   echo "$CONFSTR" >> $CFGFILE
-   FILENAME="paychan-$NET.keter"
-   tar czfv $FILENAME $BIN dist config $CFGFILE
-   maybeDeploy $1 $FILENAME
-done
+tar czfv paychan.keter $BIN dist config paychan.cfg
 
