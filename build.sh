2acfde959e40d14decd7da502e13a9740361588b
Rune K. Svendsen <runesvend@gmail.com>
2016-06-29 16:12:45 +0200

    build.sh

--- build.sh
+++ build.sh
@@ -2,14 +2,12 @@
 
 set -e
 
-apt-get install -y curl libssl-dev autoconf autogen libtool
-
-curl -sSL https://get.haskellstack.org/ | sh
+apt-get install -y curl libssl-dev autoconf autogen libtool haskell-stack
 
 git clone https://github.com/runeksvendsen/restful-payment-channel-server.git
 cd restful-payment-channel-server/
 
 stack setup
-time stack build
+stack install
 
-ls .stack-work/install/x86_64-linux/lts-5.2/7.10.3/bin
+ls /root/.local/bin
