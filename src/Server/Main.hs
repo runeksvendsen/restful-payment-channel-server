7f3df7837bc67f4f8b72a3637d825269b891a3fe
Rune K. Svendsen <runesvend@gmail.com>
2016-06-30 11:37:12 +0200

    PORT environment variable: if it parses to an unsigned integer listen on that port number

--- src/Server/Main.hs
+++ src/Server/Main.hs
@@ -6,7 +6,16 @@ import           Server.App (appInit)
 import           Snap (Config, Snap, serveSnaplet)
 import           Snap.Snaplet (runSnaplet)
 import           Snap.Http.Server (defaultConfig, httpServe)
+import           System.Environment (lookupEnv)
+import           Text.Read (readMaybe)
+import           Snap.Http.Server.Config (setPort)
 
+main :: IO ()
+main = do
+    maybePort <- return . maybe Nothing readMaybe =<< lookupEnv "PORT"
 
+    let conf = case maybePort :: Maybe Word of
+            Nothing     -> defaultConfig
+            Just port   -> setPort (fromIntegral port) defaultConfig
 
-main = serveSnaplet defaultConfig appInit
+    serveSnaplet conf appInit
