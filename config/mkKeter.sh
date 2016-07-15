e8b76986f2ee5cd4ec71f5a0bf6e52a9132ec275
Rune K. Svendsen <runesvend@gmail.com>
2016-07-15 05:45:51 +0200

    keter conf

--- config/mkKeter.sh
+++ config/mkKeter.sh
@@ -6,14 +6,17 @@ CONF=$1
 CONFDIR="$CONF/"
 
 BINNAME="PayChanServer"
+BINNAME2="ChanStore"
 BINDIR="$(stack path --local-install-root)/bin"
 
 FILENAME="paychan-$CONF.keter"
 
 cd $CONFDIR
 cp "$BINDIR/$BINNAME" ./
+cp "$BINDIR/$BINNAME2" ./
 strip $BINNAME
-tar czf ../$FILENAME $BINNAME config
+strip $BINNAME2
+tar czf ../$FILENAME $BINNAME $BINNAME2 config
 
 echo "Created $FILENAME"
 
