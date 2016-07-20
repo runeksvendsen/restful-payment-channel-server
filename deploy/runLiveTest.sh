c7d10f1bec67dae0229863841c90eac7c730a615
Rune K. Svendsen <runesvend@gmail.com>
2016-07-20 21:51:45 +0200

    no deploy

--- deploy/runLiveTest.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-screen -X -S testpay quit
-screen -X -S livepay quit
-
-screen -dmS testpay bash -c '/home/rune/.local/bin/PayChanServer -e test -p 8081'
-screen -dmS livepay bash -c  '/home/rune/.local/bin/PayChanServer -e live -p 8082'
