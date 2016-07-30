7b5733ac429afe8db21958e4d29c153a26cc21da
Rune K. Svendsen <runesvend@gmail.com>
2016-07-30 18:39:53 +0200

    Common.ResourceURL: cleanup

--- src/Common/ResourceURL.hs
+++ src/Common/ResourceURL.hs
@@ -7,34 +7,16 @@ module Common.ResourceURL where
 
 import           Common.URLParam
 
-import           Data.Bitcoin.PaymentChannel
 import           Data.Bitcoin.PaymentChannel.Types (BitcoinAmount, Payment, ChannelParameters(..),
                                                     SendPubKey, RecvPubKey, b64Encode,
                                                     IsPubKey(getPubKey))
-
-import           Data.Aeson
-    (Result(..), Value(Number, Object, String), FromJSON, ToJSON, parseJSON, toJSON,
-    fromJSON, withScientific, eitherDecodeStrict, encode, decode, (.=), (.:), object)
-
 import           Data.Bitcoin.PaymentChannel.Util
 
 import qualified Network.Haskoin.Transaction as HT
 import qualified Network.Haskoin.Crypto as HC
-import qualified Network.Haskoin.Util as HU
 import qualified Data.ByteString as BS
-import qualified Data.ByteString.Char8 as C
-import qualified Data.ByteString.Base16 as B16
-import           Data.Word (Word32)
-import           Data.EitherR (fmapL)
 import           Data.String.Conversions (cs)
 import           Text.Printf (printf)
-import qualified Data.Binary as Bin
-import           Data.Time.Clock (UTCTime)
-import           Data.Time.Clock.POSIX (utcTimeToPOSIXSeconds, posixSecondsToUTCTime)
-
-
-
-
 
 
 
@@ -56,7 +38,7 @@ activeChannelPath (HT.OutPoint txid vout)  = "/channels/" ++
     cs (pathParamEncode txid) ++ "/" ++
     cs (pathParamEncode vout)
 
---- Test URLS
+--- Test URLs
 -- /channels/new" -- ?client_pubkey&exp_time
 channelOpenURL :: Bool -> String -> BS.ByteString -> SendPubKey -> BitcoinLockTime -> String
 channelOpenURL isSecure host basePath sendPK expTime =
@@ -68,6 +50,7 @@ mkOpenQueryParams chgAddr payment =
         (cs $ pathParamEncode chgAddr :: String)
         (cs $ pathParamEncode payment :: String)
 
+-- https://localhost:8000/channels/f583e0b.../1?payment=AAf8s...(&change_address=2Nuz3s...)
 mkPaymentURL :: Bool -> String -> BS.ByteString -> HT.OutPoint -> Payment -> String
 mkPaymentURL isSecure host basePath chanId payment  =
     activeChannelURL isSecure (cs host) basePath chanId ++ mkPaymentQueryParams payment Nothing
@@ -79,10 +62,7 @@ mkPaymentQueryParams payment maybeAddr =
         (cs $ pathParamEncode payment :: String) ++
     maybe "" (\addr -> "&change_address=" ++ (cs . pathParamEncode $ addr)) maybeAddr
 
--- https://localhost/channels/f583e0b.../1
+-- https://localhost:8000/channels/f583e0b.../1
 activeChannelURL :: Bool -> BS.ByteString -> BS.ByteString -> HT.OutPoint -> String
 activeChannelURL isSecure host basePath chanId =
     channelRootURL isSecure (cs host) basePath ++ activeChannelPath chanId
---- Test URLs
-
-----URLs-----
