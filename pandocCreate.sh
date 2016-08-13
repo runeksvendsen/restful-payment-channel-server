55987b8497f0de5fbdc363f515fd2b0941fb6b71
Rune K. Svendsen <runesvend@gmail.com>
2016-08-13 08:58:59 +0200

    pandoc

--- pandocCreate.sh
+++ pandocCreate.sh
@@ -13,7 +13,7 @@ do
    echo 'Processing "' $FILE '"'
    cat $FILE | egrep -v " LANGUAGE |^module|^import" | \
       awk '!NF {if (++n <= 2) print; next}; {n=0;print}' | awk '$0="^ "$0' # | \
-      sed -r -n -e '/^import/,${p}' 
+      sed -n -e '/^import/,$p' 
       #pandoc --variable mainfont="Palatino" --variable sansfont="Helvetica" --variable monofont="Menlo" \
       #   --variable fontsize=12pt --variable version=1.17.2 --latex-engine=xelatex \
       #   -o "$(prettyPrint $FILE).pdf"
